7cf4367ff44365a6041e95d4d925dfafde09204f
Aibek2201 <aibekalmabekuly@gmail.com>
2024-05-31 23:49:26 +0500

    added some configs

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 import uvicorn
+import asyncio
+import platform
+
+
+if platform.system() == 'Windows':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
